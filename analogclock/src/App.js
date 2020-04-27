5b8d4c1a868282b93eb59dfe6ab99d273e14baae
grasyndayton <grasyn.dayton@opka.org>
2020-04-27 22:37:03 +0530

    regular clock effect and continuous clock effect implemented

--- analogclock/src/App.js
+++ analogclock/src/App.js
@@ -8,37 +8,69 @@ class App extends React.Component {
 constructor(props){
   super(props)
   this.state = {
+    regularClock:true,
     seconds:new Date().getSeconds()*6,
     minute:new Date().getMinutes()*6,
-    hour:new Date().getHours()*13,
+    hour:new Date().getHours()*30,
     date:'',
+    seconds2:new Date().getSeconds()*6,
   }
 }
 
+
+clickHandler = () => {
+  if(this.state.regularClock){
+    this.setState({
+      regularClock:false,
+      seconds2:new Date().getSeconds()*6,
+    })
+  } else if (this.state.regularClock === false){
+    this.setState({
+      regularClock:true
+    })
+  }
+}
+
+
 componentDidMount(){
 var setinterval = setInterval(()=>{
+  switch(this.state.regularClock){
+    case true:
   this.setState((prevState)=>({
     seconds:new Date().getSeconds()*6,
-    minute:this.state.seconds/60+new Date().getMinutes()*6,
-    hour:this.state.minute/60+new Date().getHours()*30,
+    minute:new Date().getMinutes()*6+this.state.seconds/60,
+    hour:new Date().getHours()*30+this.state.minute/12,
     date:new Date().getHours() + ":"+new Date().getMinutes() + ":"+ new Date().getSeconds(),
   }))
   var secondsHand = document.querySelector("#secondsHand").style.transform = "rotate("+this.state.seconds+"deg)" 
   var minute = document.querySelector("#minuteHand").style.transform = "rotate("+this.state.minute+"deg)"
   var hour = document.querySelector("#hourHand").style.transform = "rotate("+this.state.hour+"deg)"
-  var date = document.querySelector("#date").innerHTML = this.state.date
-}, 1000)
-/// ------------
+break;
+case false:
 
+this.setState((prevState)=>({
+    seconds2:prevState.seconds2+0.10,
+  }))
+  secondsHand = document.querySelector("#secondsHand").style.transform = "rotate("+this.state.seconds2+"deg)" 
+  minute = document.querySelector("#minuteHand").style.transform = "rotate("+this.state.minute+"deg)"
+  hour = document.querySelector("#hourHand").style.transform = "rotate("+this.state.hour+"deg)"
 }
+
+},18)
+
+/// ------------
+  
+}
+
+
 
 render(){
 
       return (<div className="container-fluid">
         <div className="row">
         <section className="col-6 border border-primary">
+        <button onClick={this.clickHandler}> change </button>
         <h1>analog clock</h1>
-        {this.state.hour + ":" +this.state.minute + ":"+this.state.seconds}
         <div className="clockbody">
         <img src={seconds} id="hourHand"/>
         <img src={seconds} id="minuteHand"/>
@@ -51,12 +83,8 @@ render(){
         </span>
         </div>
         </section>
-        <section className="col-6 text-center border border-danger">
+        <section className=" col-6 text-center border border-danger">
         <h1>digital clock</h1>
-        <p id="date"></p>
-        <img src={seconds} id="hourHand2"/>
-        <img src={seconds} id="minuteHand2"/>
-        <img src={seconds} id="secondsHand2"/>
         </section>
         </div>
     </div>)}
