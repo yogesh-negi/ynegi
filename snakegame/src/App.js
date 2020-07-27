48f2885ec4899fd3f76d28053c8665c715ce83b5
yogesh-negi <grasyn.dayton@opka.org>
2020-07-27 16:51:33 +0530

    snakegame project completed

--- snakegame/src/App.js
+++ snakegame/src/App.js
@@ -3,48 +3,107 @@ import './App.css';
 
 function App() {
   useEffect(()=>{
-    let inputfield = document.querySelector(".cordinates")
-    let playground = document.querySelector('.playground')
+    let playground = document.querySelector('.playground') 
     let keyrightfunction, keydownfunction, keyleftfunction, keyupfunction;
-    let snake_x_cord, snake_y_cord, food_x_cord, food_y_cord;
-    let number = 0
+    let snake_x_cord, snake_y_cord, food_x_cord, food_y_cord, currentfunction;
+    let number = 25
     let randomnumber = Math.floor(Math.random() * 625) + 1
-    let speed = 100;
-
+    let speed = 50;
+    let plagroundcords = playground.getBoundingClientRect();
+    let snakelength = 3;
+    let array = [0,25];
   for(let i=0;i<25;i++){
     for(let x=0;x<25;x++){
       let div = document.createElement("div");
       playground.appendChild(div);
-    }
+     }
   }
+  playground.focus();
+  playground.style.background = "black"
 let snake = document.querySelectorAll('.playground div');
-snake[0].style.background = "none"
+array.forEach((val)=>{
+  snake[val].style.background = "white"
+})
 let food = document.querySelectorAll('.playground div');
-food[randomnumber].style.background = "grey"
+food[randomnumber].style.background = "blue"
 
 let keydown = ()=>{
+  currentfunction = "ArrowDown"
+  array.push(number)
+  if(snake_y_cord > plagroundcords.bottom - 15) number -= 625
+  snake[array[array.length-snakelength]].style.background !== "red"?snake[array[array.length-snakelength]].style.background = "black":snake[array[array.length-snakelength]].style.background = "red"
   number+=25
-  snake[number-25].style.background = "black"
-  snake[number].style.background = "none"
-  
+  snake[number].style.background = "white"
+  if(snakelength>4){
+    for(let i=array.length;i>=array.length-snakelength;i--){
+      if(array[i] == number){
+        array.forEach((val)=>{
+          return snake[val].style.background == "white"?snake[val].style.background = "black":false
+        })
+          snakelength = 3
+          array = [array[array.length-1],array[array.length-2]]
+      }
+    }
+  }
 }
 
 let keyright = ()=>{
-  number++
-  snake[number-1].style.background = "black"
-  snake[number].style.background = "none"
+  currentfunction = "ArrowRight"
+  array.push(number)
+if(snake_x_cord>=plagroundcords.left+280) number -= 25
+snake[array[array.length-snakelength]].style.background !== "red"?snake[array[array.length-snakelength]].style.background = "black":snake[array[array.length-snakelength]].style.background = "red"
+number++
+snake[number].style.background = "white"
+if(snakelength>4){
+  for(let i=array.length;i>=array.length-snakelength;i--){
+    if(array[i] == number){
+      array.forEach((val)=>{
+        return snake[val].style.background == "white"?snake[val].style.background = "black":false
+      })
+        snakelength = 3;
+        array = [array[array.length-1],array[array.length-2]]
+    }
+  }
+}
 }
 
 let keyleft = ()=>{
+  currentfunction = "ArrowLeft"
+  array.push(number)
+  if(snake_x_cord <= plagroundcords.left) number += 25
+  snake[array[array.length-snakelength]].style.background !== "red"?snake[array[array.length-snakelength]].style.background = "black":snake[array[array.length-snakelength]].style.background = "red"
   number--
-  snake[number+1].style.background = "black"
-  snake[number].style.background = "none"
+  snake[number].style.background = "white"
+  if(snakelength>4){
+    for(let i=array.length;i>=array.length-snakelength;i--){
+      if(array[i] == number){
+        array.forEach((val)=>{
+          return snake[val].style.background == "white"?snake[val].style.background = "black":false
+        })
+        snakelength = 3;
+        array = [array[array.length-1],array[array.length-2]]
+      }
+    }
+  }
 }
-
 let keyup = ()=>{
-  number-=25
-  snake[number+25].style.background = "black"
-  snake[number].style.background = "none"
+  currentfunction = "ArrowUp"
+  array.push(number) 
+  if(snake_y_cord <= plagroundcords.top) number += 625
+snake[array[array.length-snakelength]].style.background !== "red"?snake[array[array.length-snakelength]].style.background = "black":snake[array[array.length-snakelength]].style.background = "red"  
+number-=25
+snake[number].style.background = "white"
+if(snakelength>4){
+  for(let i=array.length;i>=array.length-snakelength;i--){
+    if(array[i] == number){
+      array.forEach((val)=>{
+        return snake[val].style.background == "white"?snake[val].style.background = "black":false
+      })
+        snakelength = 3;
+        array = [array[array.length-1],array[array.length-2]]
+    }
+  }
+ }
 }
 
 function moveSnake(e){
@@ -53,25 +112,25 @@ function moveSnake(e){
     clearInterval(keyrightfunction)
     clearInterval(keyleftfunction)
     clearInterval(keyupfunction)
-    keydownfunction = setInterval(keydown,speed);
-    break;
+    return currentfunction == "ArrowUp"?keyupfunction = setInterval(keyup,speed):currentfunction!=="ArrowDown"?keydownfunction = setInterval(keydown,speed):false;
+    break ;
     case "ArrowRight":
       clearInterval(keydownfunction)
       clearInterval(keyleftfunction)
       clearInterval(keyupfunction)
-      keyrightfunction = setInterval(keyright,speed);
+      return currentfunction == "ArrowLeft"?keyleftfunction = setInterval(keyleft,speed):currentfunction!=="ArrowRight"?keyrightfunction = setInterval(keyright,speed):false
       break;
       case "ArrowLeft":
       clearInterval(keydownfunction)
       clearInterval(keyrightfunction)
       clearInterval(keyupfunction)
-      keyleftfunction = setInterval(keyleft,speed);
+      return currentfunction == "ArrowRight"?keyrightfunction = setInterval(keyright,speed):currentfunction!=="ArrowLeft"?keyleftfunction = setInterval(keyleft,speed):false;
       break;
       case "ArrowUp":
       clearInterval(keydownfunction)
       clearInterval(keyrightfunction)
       clearInterval(keyleftfunction)
-      keyupfunction = setInterval(keyup,speed);
+      return currentfunction == "ArrowDown"?keydownfunction = setInterval(keydown,speed):currentfunction!=="ArrowUp"?keyupfunction = setInterval(keyup,speed):false;
       break;
  }  
 }
@@ -82,21 +141,33 @@ setInterval(()=>{
   food_x_cord = food[randomnumber].getBoundingClientRect().x
  food_y_cord = food[randomnumber].getBoundingClientRect().y
 if(snake_x_cord === food_x_cord && snake_y_cord === food_y_cord){
-  randomnumber = Math.floor(Math.random() * 625)+1
-  food[randomnumber].style.background = 'grey';
+  snakelength++
+  do {
+    randomnumber = Math.floor(Math.random() * 625)+1
+    if(food[randomnumber].style.background == "white"){
+      randomnumber = Math.floor(Math.random() * 625)+1
+      food[randomnumber].style.background = "red"
+    } else {
+      food[randomnumber].style.background = "red"
+    }
+  } 
+  while(food[randomnumber].style.background == "white")
   }
+  document.querySelector("#score").textContent = snakelength*100-300
+
 },1)
-
-inputfield.addEventListener("keydown", moveSnake)
-
+playground.addEventListener("keydown", moveSnake)
   });
+  
   return (
     <div className="container">
-     <div className="playground">
+      <div><h1> SNAKE GAME</h1> </div>
+     <div className="playground" tabIndex="0">
      </div>
-     <input type="text" className="cordinates"/>
+     <h1 id="score"></h1>
     </div>
   );
 }
 
 export default App;
+
--- snakegame/src/external.js
+++ snakegame/src/external.js
@@ -1,55 +1,167 @@
-let playground = document.querySelector(".playground");
-let snake = document.querySelector(".snake");
-let coordinates = document.querySelector(".cordinates")
-let rectClient = snake.getBoundingClientRect()
-let xaxis = rectClient.x
-let yaxis = rectClient.y 
-let keyup, keydown, keyright, keyleft;
- 
- 
- let arrowup = ()=>{
- yaxis--
-snake.style.transform = `translate(${xaxis}px, ${yaxis}px)`;
- } 
- 
- let arrowdown = ()=>{
- yaxis++
-snake.style.transform = `translate(${xaxis}px, ${yaxis}px)`;
- }
- 
- let arrowright = ()=>{
- xaxis++
-snake.style.transform = `translate(${xaxis}px, ${yaxis}px)`;
- } 
- 
- let arrowleft = ()=>{
- xaxis--
-snake.style.transform = `translate(${xaxis}px, ${yaxis}px)`;
- }
- 
- let direction = (e)=>{
- let keypressed = e.key
- if(keypressed == "ArrowUp"){
- clearInterval(keydown)
- clearInterval(keyright)
-clearInterval(keyleft)
- keyup = setInterval(arrowup,20);
- } else if(keypressed == "ArrowDown") {
-clearInterval(keyup)
-clearInterval(keyright)
-clearInterval(keyleft)
-keydown = setInterval(arrowdown,20);
- }else if(keypressed == "ArrowLeft") {
-clearInterval(keyup)
-clearInterval(keydown)
-clearInterval(keyright)
-keyleft = setInterval(arrowleft,20);
- }else if(keypressed == "ArrowRight") {
-clearInterval(keyup)
-clearInterval(keydown)
-clearInterval(keyleft)
-keyright = setInterval(arrowright,20);
- }
- }
- 
- coordinates.addEventListener('keyup', direction)
+useEffect(()=>{
+    let inputfield = document.querySelector(".cordinates")
+    let playground = document.querySelector('.playground')
+    let keyrightfunction, keydownfunction, keyleftfunction, keyupfunction;
+    let snake_x_cord, snake_y_cord, food_x_cord, food_y_cord, currentfunction;
+    let number = 0
+    let randomnumber = Math.floor(Math.random() * 625) + 1
+    let speed = 200;
+    let plagroundcords = playground.getBoundingClientRect();
+    let snakelength = 1;
+  for(let i=0;i<25;i++){
+    for(let x=0;x<25;x++){
+      let div = document.createElement("div");
+      playground.appendChild(div);
+     }
+  }
+  playground.style.background = "black"
+let snake = document.querySelectorAll('.playground div');
+snake[0].style.background = "white"
+let food = document.querySelectorAll('.playground div');
+food[randomnumber].style.background = "grey"
+
+let keydown = ()=>{
+  currentfunction = "ArrowDown"
+  if(snake_y_cord > plagroundcords.bottom - 15) return false
+  number+=25
+
+  for(let i=0;i<number-24*snakelength;i++){
+    if(snake[i].style.background == "white")
+    {
+      snake[i].style.background = "black"
+   }
+  }
+  for(let i=number+25;i<625;i++){
+    if(snake[i].style.background == "white")
+    {
+      snake[i].style.background = "black"
+   }
+  }
+
+  snake[number-25*snakelength<0?number-25+snakelength:number-25*snakelength].style.background = "black"
+  snake[number].style.background = "white"
+  //document.querySelector("#cords").innerHTML = number;
+}
+
+let keyright = ()=>{
+  currentfunction = "ArrowRight"
+if(snake_x_cord>=plagroundcords.left+280) return false
+  number++
+  for(let i=0;i<number-24;i++){
+    if(snake[i].style.background == "white")
+    {
+      snake[i].style.background = "black"
+   }
+  }
+  for(let i=number+24;i<625;i++){
+    if(snake[i].style.background == "white")
+    {
+      snake[i].style.background = "black"
+   }
+  }
+
+  snake[number-snakelength].style.background = "black"
+  snake[number].style.background = "white"
+  //document.querySelector("#cords").innerHTML = number;
+}
+
+let keyleft = ()=>{
+  currentfunction = "ArrowLeft"
+  if(snake_x_cord <= plagroundcords.left) return false
+  number--
+  
+  for(let i=0;i<number-20;i++){
+    if(snake[i].style.background == "white")
+    {
+      snake[i].style.background = "black"
+   }
+  }
+  for(let i=number+24;i<625;i++){
+    if(snake[i].style.background == "white")
+    {
+      snake[i].style.background = "black"
+   }
+  }
+  snake[number+snakelength>624?number-25+snakelength:number+snakelength].style.background = "black"
+  snake[number].style.background = "white"
+  //document.querySelector("#cords").innerHTML = number;
+}
+
+let keyup = ()=>{
+  currentfunction = "ArrowUp"
+  if(snake_y_cord <= plagroundcords.top) return false
+    number-=25
+
+    for(let i=0;i<number-24;i++){
+      if(snake[i].style.background == "white")
+      {
+        snake[i].style.background = "black"
+     }
+    }
+    for(let i=number+24*snakelength;i<625;i++){
+      if(snake[i].style.background == "white")
+      {
+        snake[i].style.background = "black"
+     }
+    }
+    
+  snake[number+25*snakelength>600?number+25-snakelength:number+25*snakelength].style.background = "black"
+  snake[number].style.background = "white"
+//    document.querySelector("#cords").innerHTML = number;
+}
+
+function moveSnake(e){
+ switch(e.key){
+   case "ArrowDown":
+    clearInterval(keyrightfunction)
+    clearInterval(keyleftfunction)
+    clearInterval(keyupfunction)
+    return currentfunction!=="ArrowDown"?keydownfunction = setInterval(keydown,speed):false;
+    break;
+    case "ArrowRight":
+      clearInterval(keydownfunction)
+      clearInterval(keyleftfunction)
+      clearInterval(keyupfunction)
+      return currentfunction!=="ArrowRight"?keyrightfunction = setInterval(keyright,speed):false
+      break;
+      case "ArrowLeft":
+      clearInterval(keydownfunction)
+      clearInterval(keyrightfunction)
+      clearInterval(keyupfunction)
+      return currentfunction!=="ArrowLeft"?keyleftfunction = setInterval(keyleft,speed):false;
+      break;
+      case "ArrowUp":
+      clearInterval(keydownfunction)
+      clearInterval(keyrightfunction)
+      clearInterval(keyleftfunction)
+      return currentfunction!=="ArrowUp"? keyupfunction = setInterval(keyup,speed):false;
+      break;
+ }  
+}
+
+setInterval(()=>{
+  snake_x_cord = snake[number].getBoundingClientRect().x
+  snake_y_cord = snake[number].getBoundingClientRect().y
+  food_x_cord = food[randomnumber].getBoundingClientRect().x
+ food_y_cord = food[randomnumber].getBoundingClientRect().y
+if(snake_x_cord === food_x_cord && snake_y_cord === food_y_cord){
+  randomnumber = Math.floor(Math.random() * 625)+1
+  food[randomnumber].style.background = 'grey';
+  snakelength++
+  }
+},1)
+
+inputfield.addEventListener("keydown", moveSnake)
+
+  });
+  return (
+    <div className="container">
+     <div className="playground">
+     </div>
+     <input type="text" className="cordinates"/>
+     <p id="cords"></p>
+    </div>
+  );
+}
+
+export default App;
