01349d088b0f11d89330d7d6196e58b22b2c4c4e
yogesh-negi <ynegi256@gmail.com>
2021-04-19 11:17:52 +0530

    node modules deleted

--- musicproject/src/Components/songsaudiofiles.js
+++ musicproject/src/Components/songsaudiofiles.js
@@ -1,7 +1,9 @@
 import React from "react";
 import {Component} from "react"
 import jsondata from "./data.json";
-import {songslistobject} from "./importsongs"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay, faPause, faArrowRight, faArrowLeft} from '@fortawesome/free-solid-svg-icons';
+import "../App.css"
 
 class MusicPlayer extends Component{
     constructor(props){
@@ -43,10 +45,29 @@ return false
         return (
             <div>
             <section>
-                 <p>{this.props.songname}</p>
+                
             <audio name="autonext" onEnded={(e)=>{this.props.changesong(e)}} autoPlay={true} controls>
               <source type="audio/mp3" ></source>
             </audio>
+            <div className="customPlayer">
+            <p>{this.props.songname}</p>
+            <p className="currenttime"></p>
+            
+            <div>
+            <FontAwesomeIcon icon={faArrowLeft} />
+            
+            </div>
+            <div>
+            <FontAwesomeIcon icon={faPause} />
+            </div>
+            <div>
+            <FontAwesomeIcon icon={faPlay} />
+            </div>
+            <div>
+            <FontAwesomeIcon icon={faArrowRight} />
+            </div>
+            </div>
+            <input type="range"/>
             </section>
             
             </div>
